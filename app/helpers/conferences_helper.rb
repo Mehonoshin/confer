fb93d212c8e5d198591030f38bfad7070ea17207
Stanislav Mekhonoshin <ejabberd@gmail.com>
2012-12-18 23:57:11 +0400

    Таблица конференций

--- app/helpers/conferences_helper.rb
+++ app/helpers/conferences_helper.rb
@@ -1,2 +1,10 @@
 module ConferencesHelper
+  def human_date_with_time(date)
+    date.strftime("%d.%m.%Y %H:%M") if date
+  end
+
+  def human_date(date)
+    date.strftime("%d.%m.%Y") if date
+  end
+
 end
