506087d9538b449b66c480089e49d6e2ca83c752
Stanislav Mekhonoshin <ejabberd@gmail.com>
2012-12-25 00:10:14 +0400

    Простенький seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = User.create(email: "admin@vsuconf.ru", password: "1234567")
+admin.confirm!
+admin.admin!
+
+25.times do
+  user = FactoryGirl.create(:user)
+  user.confirm!
+end
+
+25.times do
+  FactoryGirl.create(:conference, user_id: User.last.id)
+end
