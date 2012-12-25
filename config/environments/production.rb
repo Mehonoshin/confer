0a3ecfb3aa56c4c0a727b8fb2098825c05eac8a3
Stanislav Mekhonoshin <ejabberd@gmail.com>
2012-12-25 20:15:52 +0400

    Правка хоста в prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Confer::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => 'sleepy-woodland-9571.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'vsuconf.ru' }
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address  => "smtp.gmail.com",
