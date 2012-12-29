8f71b77bcc6834a42467f39e2e38e1b583a48e8d
Stanislav Mekhonoshin <ejabberd@gmail.com>
2012-12-30 00:14:23 +0400

    Шаринг сессии между поддоменами

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-Confer::Application.config.session_store :cookie_store, key: '_confer_session'
+
+Confer::Application.config.session_store :active_record_store, key: '_confer_session', domain: ".vsuconf.ru" if Rails.env.production?
+Confer::Application.config.session_store :active_record_store, key: '_confer_session', domain: ".confer.dev" if Rails.env.development?
+Confer::Application.config.session_store :active_record_store, key: '_confer_session', domain: ".confer.dev" if Rails.env.development?
+
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
