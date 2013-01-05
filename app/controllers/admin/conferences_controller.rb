470bcc75e604cfc889622c03e966e99c03466e2e
Stanislav Mekhonoshin <ejabberd@gmail.com>
2013-01-06 01:38:53 +0400

    Редактирование конференции в админке

--- app/controllers/admin/conferences_controller.rb
+++ app/controllers/admin/conferences_controller.rb
@@ -13,4 +13,14 @@ class Admin::ConferencesController < Admin::BaseController
     Conference.find(params[:id]).destroy
     redirect_to admin_conferences_path, notice: t('admin.conferences.notices.removed')
   end
+
+  def edit
+    @conference = Conference.find(params[:id])
+  end
+
+  def update
+    
+    Conference.find(params[:id]).update_attributes(params[:conference])
+    redirect_to admin_conferences_path, notice: t('conferences.notices.updated')
+  end
 end
